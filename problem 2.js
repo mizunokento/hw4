f156c9ee56afd3b9fee4ca368bca5d83da2ee056
mizunokento <kmizuno@cca.edu>
2017-02-14 12:16:33 -0800

    Update problem 2.js

--- problem 2.js
+++ problem 2.js
@@ -1,5 +1,3 @@
-//Grid
-
 function setup(){
 createCanvas(400,400);
 background(0);
@@ -12,3 +10,16 @@ var ellipseX = 17;
 var ellipseY = 17;
 
 fill(255);
+
+
+while (ellipseX<390){
+
+     while (ellipseY<390){
+     ellipse(ellipseX, ellipseY, ellipseR, ellipseR);
+     ellipseY=ellipseY+16
+     }
+     ellipseY=18;
+          ellipse(ellipseX, ellipseY, ellipseR, ellipseR);
+     ellipseX=ellipseX+16
+     }
+}
