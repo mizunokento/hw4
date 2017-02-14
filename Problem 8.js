d1dc1d4fb0e2e824c9b418914746956d2dd16f88
mizunokento <kmizuno@cca.edu>
2017-02-14 12:55:52 -0800

    Create Problem 8.js

---
+++ Problem 8.js
@@ -0,0 +1,21 @@
+//Diagonal Lines
+
+function setup() {
+  createCanvas(400, 400);
+     background(0);
+
+function drawlines(){     
+     strokeWeight(2);
+     stroke(255, 204, 0);
+     for(var i=25;i<375;i=i+10){
+         
+          line(25,i,i,25);
+     }
+     for(var i=25;i<375;i=i+10){
+         
+          line(i,375,375,i);
+     }
+     
+}
+     drawlines();
+}
