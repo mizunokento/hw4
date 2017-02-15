63781ae289ab3a8b28dea6336be9eb088c60ca70
mizunokento <kmizuno@cca.edu>
2017-02-15 09:17:46 -0800

    Update Problem 8.js

--- Problem 8.js
+++ Problem 8.js
@@ -2,20 +2,13 @@
 
 function setup() {
   createCanvas(400, 400);
-     background(0);
-
-function drawlines(){     
+     background(0);     
      strokeWeight(2);
      stroke(255, 204, 0);
      for(var i=25;i<375;i=i+10){
-         
-          line(25,i,i,25);
+     line(25,i,i,25);
      }
      for(var i=25;i<375;i=i+10){
-         
-          line(i,375,375,i);
+         line(i,375,375,i);
      }
-     
-}
-     drawlines();
 }
